dfac4075e5b1a2dd8cabcf8e3ebe82452e4153d6
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2025-05-27 17:55:19 -0400

    Prototype API with in-memory database

--- Ephemeral.Api/Program.cs
+++ Ephemeral.Api/Program.cs
@@ -1,6 +1,46 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Ephemeral.Api;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+const string databaseName = "Secrets";
+const int minLifetime = 0;
+const int maxLifetime = 60 * 60 * 24;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SecretDb>(opt => opt.UseInMemoryDatabase(databaseName));
+
+var app = builder.Build();
+app.MapPut("/secrets", SetSecret);
+app.MapGet("/secrets/{id}", GetSecret);
 
 app.Run();
+
+// Conceal secret with specified ciphertext and time-to-live.
+static async Task<IResult> SetSecret(
+	[FromQuery(Name = "ciphertext")] string ciphertext,
+	[FromQuery(Name = "ttl")] int ttl,
+	[FromServices] SecretDb db)
+{
+	ttl = Math.Max(minLifetime, Math.Min(ttl, maxLifetime));
+
+	var secret = new Secret(ciphertext, TimeSpan.FromSeconds(ttl));
+	var task = await db.Secrets.AddAsync(secret);
+	await db.SaveChangesAsync();
+
+	return Results.Ok(task.Entity.Id);
+}
+
+// Reveal secret if still unread and unexpired.
+static async Task<IResult> GetSecret(
+	[FromRoute] int id,
+	[FromServices] SecretDb db)
+{
+	var secret = await db.Secrets.FindAsync(id);
+	if (secret is null || secret.IsRead || secret.Expiration < DateTime.UtcNow)
+		return Results.NotFound();
+
+	secret.IsRead = true;
+	await db.SaveChangesAsync();
+
+	return Results.Ok(secret.Ciphertext);
+}
---
+++ Ephemeral.Api/Secret.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ephemeral.Api
+{
+	/// <summary>
+	/// Represents a database session used to query and save <see cref="Secret"/> instances.
+	/// </summary>
+	/// <param name="options">The options for this context.</param>
+	public class SecretDb(DbContextOptions<SecretDb> options) : DbContext(options)
+	{
+		public DbSet<Secret> Secrets => Set<Secret>();
+	}
+
+	/// <summary>
+	/// Represents a single-use and self-destructing secret.
+	/// </summary>
+	public class Secret(string ciphertext, TimeSpan ttl)
+	{
+		[Key] public Guid Id { get; set; } = new();
+		public string Ciphertext { get; set; } = ciphertext;
+		public bool IsRead { get; set; } = false;
+		public DateTime Expiration { get; set; } = DateTime.UtcNow + ttl;
+	}
+}
