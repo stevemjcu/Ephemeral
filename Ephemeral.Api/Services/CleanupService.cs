bd2f209c9303540971a033df8949fcf7140b805b
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2025-05-28 23:11:35 -0400

    Simplify cleanup service

--- Ephemeral.Api/Services/CleanupService.cs
+++ Ephemeral.Api/Services/CleanupService.cs
@@ -3,45 +3,22 @@
 namespace Ephemeral.Api.Services
 {
 	/// <summary>
-	/// Represents a recurring background job to clean up expired secrets.
+	/// Represents an ongoing background job to clean up expired secrets.
 	/// </summary>
-	public class CleanupService(IServiceProvider services) : BackgroundService, IDisposable
+	public class CleanupService(IServiceProvider services) : BackgroundService
 	{
 		private readonly IServiceProvider _services = services;
-		private readonly PeriodicTimer _timer = new(TimeSpan.FromMinutes(5));
-		private bool _disposed;
 
 		protected async override Task ExecuteAsync(CancellationToken ct)
 		{
-			while (await _timer.WaitForNextTickAsync(ct))
+			using var timer = new PeriodicTimer(TimeSpan.FromMinutes(5));
+
+			while (await timer.WaitForNextTickAsync(ct))
 			{
 				using var scope = _services.CreateScope();
-				var service = scope.ServiceProvider.GetRequiredService<SecretService>();
-				await service.Secrets.Where(s => s.Expiration < DateTime.UtcNow).ExecuteDeleteAsync(ct);
+				var db = scope.ServiceProvider.GetRequiredService<SecretService>();
+				await db.Secrets.Where(s => s.Expiration < DateTime.UtcNow).ExecuteDeleteAsync(ct);
 			}
-		}
-
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!_disposed)
-			{
-				if (disposing)
-				{
-					// Dispose managed resources
-					_timer.Dispose();
-				}
-
-				// Free unmanaged resources
-				// Null large fields
-				_disposed = true;
-			}
-		}
-
-		public override void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-			base.Dispose();
 		}
 	}
 }
