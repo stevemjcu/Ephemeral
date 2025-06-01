4a814d6543b814502434bcb1a9f3906bf1db96e3
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2025-06-01 19:16:33 -0400

    Add service reference via OpenAPI doc

--- Ephemeral.App/Ephemeral.App.Client/Program.cs
+++ Ephemeral.App/Ephemeral.App.Client/Program.cs
@@ -1,5 +1,12 @@
+using Ephemeral.App.Client;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped(sp =>
+	new EphemeralService(new HttpClient
+	{
+		BaseAddress = new Uri(builder.Configuration["EphemeralServiceUri"]!)
+	}));
 
 await builder.Build().RunAsync();
