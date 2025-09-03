3ff6e24a497af6ac191bc6767a6f5baae144c8d5
Stephen Justice <43153897+stevemjcu@users.noreply.github.com>
2025-09-03 19:43:09 -0400

    Add defaults/checks to API

--- Ephemeral.Api/Program.cs
+++ Ephemeral.Api/Program.cs
@@ -4,8 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 internal class Program
 {
+	static readonly TimeSpan DefaultLifetime = TimeSpan.FromMinutes(5);
 	static readonly TimeSpan MinLifetime = TimeSpan.FromSeconds(0);
 	static readonly TimeSpan MaxLifetime = TimeSpan.FromDays(1);
+	static readonly int MaxLength = 512;
 
 	private static void Main(string[] args)
 	{
@@ -15,16 +17,16 @@ internal class Program
 		var frontendUri = builder.Configuration["FrontendUri"]!;
 		var cleanupInterval = TimeSpan.Parse(builder.Configuration["CleanupInterval"]!);
 
-		builder.Services.AddCors(
-			options => options.AddDefaultPolicy(
-				policy => policy
-					.WithOrigins(frontendUri)
-					.AllowAnyMethod()
-					.AllowAnyHeader()));
-
-		builder.Services.AddOpenApi();
-		builder.Services.AddSqlite<SecretDb>(connectionString);
-		builder.Services.AddHostedService(provider => new SecretCleanupJob(provider, cleanupInterval));
+		builder.Services
+			.AddCors(
+				options => options.AddDefaultPolicy(
+					policy => policy
+						.WithOrigins(frontendUri)
+						.AllowAnyMethod()
+						.AllowAnyHeader()))
+			.AddOpenApi()
+			.AddSqlite<SecretDb>(connectionString)
+			.AddHostedService(provider => new SecretCleanupJob(provider, cleanupInterval));
 
 		var app = builder.Build();
 
@@ -41,14 +43,16 @@ internal class Program
 	[EndpointDescription("Sets a secret with the specified ciphertext and time-to-live")]
 	[Produces<Guid>()]
 	static async Task<IResult> SetSecret(
-	[FromQuery(Name = "ciphertext")] string ciphertext,
-	[FromQuery(Name = "lifetime")] TimeSpan lifetime,
-	[FromServices] SecretDb database)
+		[FromQuery(Name = "ciphertext")] string ciphertext,
+		[FromQuery(Name = "lifetime")] TimeSpan? lifetime,
+		[FromServices] SecretDb database)
 	{
-		if (lifetime < MinLifetime || lifetime > MaxLifetime)
-			return Results.BadRequest();
+		lifetime ??= DefaultLifetime;
 
-		var secret = new Secret(ciphertext, lifetime);
+		if (lifetime < MinLifetime || lifetime > MaxLifetime) return Results.BadRequest();
+		if (ciphertext.Length > MaxLength) return Results.BadRequest();
+
+		var secret = new Secret(ciphertext, (TimeSpan)lifetime);
 		var entry = await database.Secrets.AddAsync(secret);
 		await database.SaveChangesAsync();
 
